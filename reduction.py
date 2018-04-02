b32e3594d517aa281547d8ae538c5dad7eb7b588
Ben Kaiser <bkaiser94@gmail.com>
2018-04-02 08:29:09 -0400

    removed the gaussian refitting in anticipation of the implementation of offsetting

--- reduction.py
+++ reduction.py
@@ -17,7 +17,8 @@ flatlistname = 'listFlat'
 speclistname= 'listSpec'
 
 
-linefilename = 'FeAr_3650to5250_lines.txt'
+#linefilename = 'FeAr_3650to5250_lines.txt'
+linefilename = 'JJ_FeAr_lines.txt'
 flatlist= np.genfromtxt(flatlistname,dtype = 'str' )
 zerolist = np.genfromtxt(zerolistname, dtype ='str')
 speclist= np.genfromtxt(speclistname, dtype = 'str')
@@ -39,12 +40,12 @@ bkg_shift= 50
 #line_sides= np.array([4,4,4,4,4])
 #line_x_checks= np.array([175, 728, 883, 893, 1490]) #x-values that are approximately in the middle of the lamp lines so we know where to look for centroiding
 balmer_rest_waves= np.array([4101.734, 4340.472, 4861.35]) #Only 3 Balmer Lines seem to be in this image.
-balmer_x_checks= np.array([610 ,896, 1510])   #wavelength values that should be checked for the Balmer emission lines in our spectrum (1360 line should be 30 pixels on either side)
+balmer_x_checks= np.array([610 ,890, 1510])   #wavelength values that should be checked for the Balmer emission lines in our spectrum (1360 line should be 30 pixels on either side)
 balmer_line_sides= np.array([30,30, 31]) #Number of pixels on either side of the guessed peak that should be included in the centroiding effort for the balmer lines (those are based on attempted lines).
 
 ###
 lamp_sigma_guess = 3
-line_search_width= 12
+line_search_width= 3
 balmer_sigma_guess= 7
 lamp_p0 = [1, 500,  lamp_sigma_guess, 0]
 #balmer_p0= [-1, 500, balmer_sigma_guess,balmer_line_sides[0],0]
@@ -55,48 +56,48 @@ n_trials= 1000
 ########
 #iron lines from red_cam_pipeline
 
-#WaveList_Fe_930_12_24= np.array([ [431.795, 1057.76, 1194.97, 1315.35, 
-                                   #1381.1, 1444.58,  1630.61, 
-                                   #1682.99, 1726.03, 1779.61, 
-                                   #1893.19, 2132.53, 2210.85, 
-                                   #2279.64, 2361.34, 2443.06, 2468.22, 
-                                   #2515.14, 2630.53, 2795.45, 2886.45, 
-                                   #2985.15, 3085.52, 3162.56,  
-                                   #3367.86, 3795.76, 3845.65, 
-                                   #3907.57], 
-                                   
-                                   #[3729.3087, 3994.7918, 4052.9208, 4103.9121, 
-                                    #4131.7235, 4158.5905,  4237.2198, 
-                                    #4259.3619, 4277.5282, 4300.1008, 
-                                    #4348.064, 4448.8792, 4481.8107, 
-                                    #4510.7332, 4545.0519, 4579.3495, 4589.8978, 
-                                    #4609.5673, 4657.9012, 4726.8683, 4764.8646, 
-                                    #4806.0205, 4847.8095, 4879.8635, 
-                                    #4965.0795, 5141.7827, 5162.2846, 
-                                    #5187.7462] ]) 
-                                   
-WaveList_Fe_930_12_24= np.array([ [431.795, 1057.76,  1315.35, 
-                                   1444.58,  1630.61, 
-                                   1682.99, 1726.03, 
-                                   2132.53,  
-                                   2279.64, 2361.34,  
-                                   2886.45, 
-                                   2985.15, 3085.52,   
+WaveList_Fe_930_12_24= np.array([ [431.795, 1057.76, 1194.97, 1315.35, 
+                                   1381.1, 1444.58,  1630.61, 
+                                   1682.99, 1726.03, 1779.61, 
+                                   1893.19, 2132.53, 2210.85, 
+                                   2279.64, 2361.34, 2443.06, 2468.22, 
+                                   2515.14, 2630.53, 2795.45, 2886.45, 
+                                   2985.15, 3085.52, 3162.56,  
                                    3367.86, 3795.76, 3845.65, 
                                    3907.57], 
                                    
-                                   [3729.3087, 3994.7918,  4103.9121, 
-                                    4158.5905,  4237.2198, 
-                                    4259.3619, 4277.5282, 
-                                    4448.8792,  
-                                    4510.7332, 4545.0519, 
-                                    4764.8646, 
-                                    4806.0205, 4847.8095,  
+                                   [3729.3087, 3994.7918, 4052.9208, 4103.9121, 
+                                    4131.7235, 4158.5905,  4237.2198, 
+                                    4259.3619, 4277.5282, 4300.1008, 
+                                    4348.064, 4448.8792, 4481.8107, 
+                                    4510.7332, 4545.0519, 4579.3495, 4589.8978, 
+                                    4609.5673, 4657.9012, 4726.8683, 4764.8646, 
+                                    4806.0205, 4847.8095, 4879.8635, 
                                     4965.0795, 5141.7827, 5162.2846, 
                                     5187.7462] ]) 
-line_x_checks = WaveList_Fe_930_12_24[0]/2.
-lamp_lines = WaveList_Fe_930_12_24[1]
-line_sides = np.ones(line_x_checks.shape[0])*4.
+                                   
+#WaveList_Fe_930_12_24= np.array([ [431.795, 1057.76,  
+                                   #1444.58,  1630.61, 
+                                   #1726.03, 
+                                   #2132.53,  
+                                   #2279.64, 2361.34,  
+                                   #2886.45, 
+                                   #2985.15, 3085.52,   
+                                   #3367.86, 3795.76, 3845.65, 
+                                   #3907.57], 
+                                   
+                                   #[3729.3087, 3994.7918,   
+                                    #4158.5905,  4237.2198, 
+                                    #4277.5282, 
+                                    #4448.8792,  
+                                    #4510.7332, 4545.0519, 
+                                    #4764.8646, 
+                                    #4806.0205, 4847.8095,  
+                                    #4965.0795, 5141.7827, 5162.2846, 
+                                    #5187.7462] ]) 
+#line_x_checks = WaveList_Fe_930_12_24[0]/2.
+#lamp_lines = WaveList_Fe_930_12_24[1]
+#line_sides = np.ones(line_x_checks.shape[0])*4.
 
 #fear_array = np.genfromtxt(linefilename, names= True)
 #line_x_checks = np.int_(fear_array['Pixel']/8000.*(trace_xend-trace_xstart)) #rescaled to match the dimensions of GOODMAN
@@ -105,6 +106,13 @@ line_sides = np.ones(line_x_checks.shape[0])*4.
 #line_x_checks = line_x_checks[good_fits]
 #lamp_lines= lamp_lines[good_fits]
 #line_sides = np.ones(line_x_checks.shape[0])*4.
+
+fear_array= np.genfromtxt(linefilename, names = True)
+line_x_checks = np.copy(fear_array['Pixel']) +91
+lamp_lines = np.copy(fear_array['User'])
+line_sides = np.ones(line_x_checks.shape[0])*4.
+
+
 def make_image_stack(imagelist):
     """
     
@@ -181,7 +189,6 @@ plt.plot(x_positions,target_light,'-')
 plt.xlabel('x (pixel)')
 plt.ylabel('Counts')
 plt.title('Target Spectrum')
-
 plt.show()
 
 target_light= target_light-bkg_light
@@ -196,13 +203,26 @@ plt.plot(x_positions,bkg_light,'-')
 plt.xlabel('x (pixel)')
 plt.ylabel('Counts')
 plt.title('Sky Background Spectrum')
-
 plt.show()
+####
 
-#for x_spot in line_x_checks:
-    #plt.axvline( x= x_spot, color = 'k')
-for x_spot in np.array(WaveList_Fe_930_12_24[0])/2.:
+def impose_floor(input_light, floor_cutoff):
+    """
+    basically shifts the whole spectrum down in brightness by a constant value and then sets all negative values to zero
+    """
+    output_light = np.copy(input_light)-floor_cutoff
+    ignore = np.where(output_light < 0.)
+    output_light[ignore]= 0.
+    return output_light
+######
+
+#lamp_light= impose_floor(lamp_light, 0.125*np.max(lamp_light))
+
+
+for x_spot in line_x_checks:
     plt.axvline( x= x_spot, color = 'r')
+#for x_spot in np.array(WaveList_Fe_930_12_24[0])/2.:
+    #plt.axvline( x= x_spot, color = 'r')
 plt.plot(x_positions,lamp_light,'-')
 plt.xlabel('x (pixel)')
 plt.ylabel('Counts')
@@ -212,6 +232,7 @@ plt.show()
 
 ####
 ####
+
 def gaussian_curve(x, a, x0, sigma,b):
     return a*np.exp(-(x-x0)**2/(2*sigma**2))+b
 
@@ -228,21 +249,21 @@ def fit_gaussian_curve(x_pixels, light_values, p0_list, search_width):
     print np.min(cut_x_pixels), np.max(cut_x_pixels), p0_list[1]
     cut_light_values= high_light_values[upper_cut]
     popt, pcov = sciop.curve_fit(gaussian_curve, cut_x_pixels, cut_light_values, p0= p0_list)
-    try:
-        cut_region = np.where(x_pixels> (popt[1]-search_width ))
-        print popt
-        print  "lower bound:", popt[1]-search_width
-        print "upper bound: ", popt[1]+search_width
-        high_x_pixels= np.copy(x_pixels[cut_region])
-        high_light_values= np.copy(light_values[cut_region])
-        upper_cut = np.where(high_x_pixels < (popt[1]+search_width))
-        cut_x_pixels = high_x_pixels[upper_cut]
-        print np.min(cut_x_pixels), np.max(cut_x_pixels), popt[1]
-        cut_light_values= high_light_values[upper_cut]
-        popt, pcov = sciop.curve_fit(gaussian_curve, cut_x_pixels, cut_light_values, p0= popt)
-    except ValueError as error:
-        print "probably chose a center outside the bounds of the image"
-        print error
+    #try:
+        #cut_region = np.where(x_pixels> (popt[1]-search_width ))
+        #print popt
+        #print  "lower bound:", popt[1]-search_width
+        #print "upper bound: ", popt[1]+search_width
+        #high_x_pixels= np.copy(x_pixels[cut_region])
+        #high_light_values= np.copy(light_values[cut_region])
+        #upper_cut = np.where(high_x_pixels < (popt[1]+search_width))
+        #cut_x_pixels = high_x_pixels[upper_cut]
+        #print np.min(cut_x_pixels), np.max(cut_x_pixels), popt[1]
+        #cut_light_values= high_light_values[upper_cut]
+        #popt, pcov = sciop.curve_fit(gaussian_curve, cut_x_pixels, cut_light_values, p0= popt)
+    #except ValueError as error:
+        #print "probably chose a center outside the bounds of the image"
+        #print error
         
     print popt
     print '========'
@@ -272,13 +293,16 @@ wave_peaks_found = []
 for lamp_line_guess,lamp_line_wave in zip( line_x_checks,lamp_lines):
     try:
         lamp_params, lamp_cov = fit_gaussian_curve(x_positions, lamp_light, [lamp_p0[0], lamp_line_guess, lamp_p0[2], lamp_p0[3]], line_search_width)
-        peaks_found.append(lamp_params[1])
-        wave_peaks_found.append(lamp_line_wave)
-        plt.plot(x_positions, lamp_light, label = 'lamp data', color = 'blue')
-        plt.plot(x_positions, gaussian_curve(x_positions, lamp_params[0], lamp_params[1], lamp_params[2], lamp_params[3]), color = 'r', label = 'Gaussian Fit')
-        plt.title("guess: " + str(lamp_line_guess) + ' fit:' + str(lamp_params[1]))
-        plt.legend()
-        plt.show()
+        if ((np.abs(lamp_params[0]) > 1.) and (np.abs(lamp_params[2])< 20) ):
+            peaks_found.append(lamp_params[1])
+            wave_peaks_found.append(lamp_line_wave)
+            plt.plot(x_positions, lamp_light, label = 'lamp data', color = 'blue')
+            plt.plot(x_positions, gaussian_curve(x_positions, lamp_params[0], lamp_params[1], lamp_params[2], lamp_params[3]), color = 'r', label = 'Gaussian Fit')
+            plt.title("guess: " + str(lamp_line_guess) + ' fit:' + str(lamp_params[1]))
+            plt.legend()
+            plt.show()
+        else:
+            print "Gaussian too flat:", lamp_params[0], lamp_params[2]
     except RuntimeError as error:
         print error
 
@@ -337,7 +361,7 @@ balmer_centroids= line_centroiding(target_light, balmer_x_checks, balmer_line_si
 balmer_centers = []
 for balmer_line_x, balmer_wave in zip(balmer_x_checks, balmer_rest_waves):
     try:
-        balmer_params, balmer_cov = fit_gaussian_curve(x_positions, target_light, [balmer_p0[0], balmer_line_x, balmer_p0[2], balmer_p0[3]], line_search_width)
+        balmer_params, balmer_cov = fit_gaussian_curve(x_positions, target_light, [balmer_p0[0], balmer_line_x, balmer_p0[2], balmer_p0[3]], line_search_width*3)
         balmer_centers.append(balmer_params[1])
         plt.plot(x_positions, target_light, label = 'observed', color = 'blue')
         plt.plot(x_positions, gaussian_curve(x_positions,balmer_params[0], balmer_params[1], balmer_params[2], balmer_params[3]), color = 'r', label = 'Gaussian Fit')
