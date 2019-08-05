fixed (I think) the averaging filename matching because it just was ignoring all of the end matches is there was previously a set of exposures matching that name. Also made the SLIT header reaction in...

flux_calibration.py
@@ -32,7 +32,8 @@ import get_cal_params as gcp
 #poly_degree=7
 poly_degree=7
 model_poly_degree= 5
-
+sens_fit_method='poly/poly' #400M2 method generally, gets sens curve by dividing the polynomial observed by polynomial model
+#sens_fit_method='empirical' #400M1 method generally, gets sens curve by dividing the obs flux directly by the model flux and then fitting a polynomial
 division_extra_deg = 2
 
 ext_corr=True #correct extinction
@@ -324,9 +325,16 @@ calc_waves=np.linspace(min_wave, max_wave,1000)
 sens_curve_points= np.polyval(obs_curve,calc_waves)/np.polyval(model_curve, calc_waves)
 #sens_curve_fit= np.polyfit(stand_waves, sens_curve_points,5)
 #sens_curve_fit= np.polyfit(obs_waves, sens_curve_points,poly_degree)
-sens_curve_fit= np.polyfit(calc_waves, sens_curve_points,poly_degree+2)
+#sens_curve_fit= np.polyfit(calc_waves, sens_curve_points,poly_degree+2)
 #sens_curve_fit= np.polyfit(calc_waves, sens_curve_points,cp.flux_cal_dict['obs_poly_degree'][setup_name]+division_extra_deg)
 
+if sens_fit_method == 'poly/poly':
+    sens_curve_fit= np.polyfit(calc_waves, sens_curve_points,poly_degree+2)
+elif sens_fit_method=='empirical':
+    sens_curve_fit=np.polyfit(obs_waves, obs_flux/stand_flux,poly_degree) #20190618
+else:
+    print('no valid sens_fit_method selected, currently selected:')
+    print(sens_fit_method)
 #sens_curve_fit=np.polyfit(obs_waves, obs_flux/stand_flux,poly_degree) #20190618
 
 

plot_spec.py
@@ -36,7 +36,7 @@ test_wavelength = 4686
 test_width = 40
 test_side = test_width/2
 
-pix_width=10 
+pix_width=10
 sdss_pix_width = 10
 
 #wavelength_offset=60
@@ -53,27 +53,27 @@ wavelength_offset=0
 sdss_path = '/Users/BenKaiser/Desktop/SDSS_speclib/'
 #sdss_path = '/Users/BenKaiser/Desktop/SDSS_speclib/G0_K5/'
 #print(filenames)
-plot_wavelength=True
+plot_wavelength=False
 plot_400m2_tell= False
 #norm_range=[1240,1280]
 #norm_range=[1560,1590]
 #norm_range=[40,80]
 
 #norm_range=[7042,7046]
-#norm_range=[7490,7510] #outside telluric
+norm_range=[7490,7510] #outside telluric
 #norm_range=[7470, 7530]
 #norm_range=[7517,7556] #20190528
 #norm_range=[8074,8140]
 #norm_range=[8058,8231]
 #norm_range=[5100,5400]
 #norm_range=[6090,6240]
-norm_range=[6640,6670]#20190530 400M1 norm range
+#norm_range=[6640,6670]#20190530 400M1 norm range
 ####norm_range=np.array(norm_range)+wavelength_offset
 
 #file_setting='all_avg'
 #file_setting='command' #this is essentially the version for comparing 2 goodman spectra to each other
-#file_setting='all_wctb'
-file_setting='all_fwctb'
+file_setting='all_wctb'
+#file_setting='all_fwctb'
 #file_setting= 'compare_SDSS'
 #file_setting= 'compare_only_SDSS' #this should compare the spectra beginning with 'sdss' to other objects
 #file_setting= 'all_SDSS'
@@ -86,9 +86,9 @@ double_iterate= False #file_settings change these in their little sections ahead
 
 if file_setting=='all_avg':
     print(file_setting)
-    #filenames=glob('avg_fwctb*')
+    filenames=glob('avg_fwctb*Gaia*')
     #filenames=glob('avg_fwctb*eg274*fits')
-    filenames=glob('avg_fwctb*aia*1644*fits')
+    #filenames=glob('avg_fwctb*aia*1644*fits')
     #filenames=glob('avg_wctb*fits')
     #filenames=glob('avg_fwctb*NaD*fits')
     #filenames=glob('avg_fwctb*SDSSJ1252*')
@@ -99,7 +99,7 @@ if file_setting=='all_avg':
 
 elif file_setting=='all_wctb':
     print(file_setting)
-    filenames=glob('wctb*')
+    filenames=glob('wctb*Feige110_*')
     #filenames=glob('wctb*aia*1644*')
     single_iterate=True
     double_iterate=False
@@ -107,7 +107,7 @@ elif file_setting=='all_wctb':
 
 elif file_setting=='all_fwctb':
     print(file_setting)
-    filenames=glob('fwctb*aia*1644*')
+    filenames=glob('fwctb*Feige110_*')
     #filenames=glob('fwctb*aia*1644*')
     #filenames=glob('fwctb*2356*')
     single_iterate=True
@@ -153,7 +153,8 @@ elif file_setting== 'two_arm':
     
 elif file_setting=='all_super':
     print(file_setting)
-    filenames=glob('super_fwctb*1644*fits')
+    #filenames=glob('super_fwctb*1644*fits')
+    filenames=glob('super_fwctb*Gaia*fits')
     single_iterate=True
     double_iterate=False
     
@@ -513,7 +514,7 @@ if single_iterate:
         target_spec, header, target_noise= spt.retrieve_spec(filename)
         target_spec[0]=target_spec[0]+wavelength_offset
         #conv_spec= convolve_spectrum(target_spec, header)
-        #plot_spectrum(target_spec, filename, header, smooth=True, norm=False, kernel_type='box')
+        plot_spectrum(target_spec, filename, header, smooth=True, norm=True, kernel_type='box')
         #plot_spectrum(target_spec, filename, header, smooth=True, norm=True)
         #target_spec[1]=header['airmass']
         #plot_spectrum(target_spec, filename, header, norm=False, smooth=True, kernel_type='box', pix_width=10)
@@ -527,7 +528,7 @@ if single_iterate:
         #else:
             #pass
         #plot_SNR(target_spec, target_noise, filename)
-        plot_dwavelength(target_spec, filename)
+        #plot_dwavelength(target_spec, filename)
         #spt.show_plot(show_telluric=False, show_legend=False)
         #spt.show_plot()
         #plt.legend()
@@ -537,11 +538,11 @@ if single_iterate:
             plt.title(header['airoftyp'])
         except KeyError:
             pass
-    #spt.show_plot(show_legend=True)
+    spt.show_plot(show_legend=False)
     #spt.show_plot(show_legend=False, show_telluric=False)
     #spt.show_plot(show_telluric=False)
-    plt.legend()
-    plt.show()
+    #plt.legend()
+    #plt.show()
     #plot_pix_shifts(filenames)
 else:
     pass

wave_cal.py
@@ -79,7 +79,7 @@ trace_offset =0  #amount by which the calculated trace needs to be offset to end
 #trace_band_mid= 95   #y-pixel that's about the center of the trace J1431
 #trace_band_mid=105 #y-pixel for Keaton's object 2019-03-07 2019-03-25 commented out
 #trace_band_mid=110
-trace_band_mid=105
+trace_band_mid=81
 #trace_band_mid= 112 #y-pixel for SDSSJ1159 400M1
 #trace_band_mid= 90 #y-pixel for SDSSJ1159 400M2
 #trace_band_mid=60 #
@@ -92,7 +92,7 @@ trace_band_width = 50#pixel width to determine the center of the trace 2019-03-2
 #trace_band_mid=115 #y-pixel for actual wisea0615
 #trace_band_width = 10 #pixel width to determine the center of the trace
 #sigma_multi_side= 4 #multiple of sigma value of trace gaussian that should be distance out to go for extraction window
-sigma_multi_side= 3 #multiple of sigma value of trace gaussian that should be distance out to go for extraction window
+sigma_multi_side= 2 #multiple of sigma value of trace gaussian that should be distance out to go for extraction window
 
 core_sides=  5
 #core_sides=  7
@@ -249,8 +249,15 @@ def fit_gaussian_curve(x_pixels, light_values, p0_list, search_width, plot_all =
     return popt, pcov
 
 def get_skyline_bounds(header, x_pixels):
-    slit_width= header['SLIT'].split('"')[0] #separate the slit header to get the arcsecond value of the slit width
-    slit_width = float(slit_width) #it was a string
+    try:
+        slit_width= header['SLIT'].split('"')[0] #separate the slit header to get the arcsecond value of the slit width
+        slit_width = float(slit_width) #it was a string
+    except ValueError as error:
+        print('ValueError:', error)
+        print('The Goodman headers were changed for no apparent reason on 2019-07-30, so we have to try another format for the slit width')
+        slit_width= header['SLIT'].split('_')[0] 
+        slit_width = float(slit_width) #it was a string
+    #slit_width = float(slit_width) #it was a string
     xpix_scale, ypix_scale= spt.get_pixel_scale(header)
     slit_width_pix= slit_width/xpix_scale
     skyline_bounds={
@@ -371,10 +378,16 @@ def find_skyline_offset(x_pixels, light_values, airline_lambda, wave_coeffs, hea
     else:
         pass
     p0_dict['x_0']= max_point
-    corr_pixel= fit_slitskyline_function(cut_x_pixels, cut_light_values, header, p0_dict= p0_dict, plot_all=plot_all)
-    corr_wave= np.polyval(wave_coeffs, corr_pixel) #''wavelength'' of skyline on original scale
-    offset_lambda= airline_lambda-corr_wave
-    offset= airline_guess-corr_pixel #offset that should be added to the pixel values for the new determinations
+    try:
+        corr_pixel= fit_slitskyline_function(cut_x_pixels, cut_light_values, header, p0_dict= p0_dict, plot_all=plot_all)
+        corr_wave= np.polyval(wave_coeffs, corr_pixel) #''wavelength'' of skyline on original scale
+        offset_lambda= airline_lambda-corr_wave
+        offset= airline_guess-corr_pixel #offset that should be added to the pixel values for the new determinations
+    except RuntimeError as error:
+        print(error)
+        print('automatically setting this one skyline offset as 0')
+        offset_lambda=0
+        offset=0
     return offset, offset_lambda
 
 