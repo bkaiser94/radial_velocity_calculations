correct error combinations for the rebinned average spectra now.

atmos_corr.py
@@ -32,6 +32,7 @@ import get_cal_params as gcp
 
 #sens_names = glob('E*sensitivity_curve.txt')+glob('G*sensitivity_curve.txt')+glob('e*sens*curve.txt')
 sens_names=glob('sens_curv*')
+#sens_names=glob('sens*')
 #sens_names = glob('*sensitivity*.txt')
 
 resid_names= glob('resid*.txt')
@@ -39,8 +40,9 @@ resid_names= glob('resid*.txt')
 tell_names= glob('tell*')
 do_fnu=False
 
-wavelengths = np.linspace(4940,8980, 8080) #400M2 approximately
+#wavelengths = np.linspace(4940,8980, 8080) #400M2 approximately
 #wavelengths = np.linspace(3800,7200, 8080) #400M1 approximately
+wavelengths= np.linspace(3800, 8980, 10000)
 
 
 def extract_AM_MJD(sens_curve_file):
@@ -131,17 +133,19 @@ plt.xlabel('airmass')
 plt.ylabel('max throughput of sensitivity curve')
 plt.show()
 
-#for resid_name in resid_names:
-    #airmass, mjd= extract_AM_MJD(resid_name)
-    #resid_array = np.genfromtxt(resid_name)
-    #max_index= np.argmax(sens_curve)
-    #label= ','.join([resid_name, str(airmass), str(mjd)])
-    #plt.plot(resid_array, label=label)
-    #print("\n=============")
-    #print(sens_name)
-    #print('airmass:', airmass, 'mjd:', mjd)
-#plt.xlabel(r'Pixel')
-#spt.show_plot()
+for resid_name in resid_names:
+    header_dict= extract_AM_MJD(resid_name)
+    airmass= header_dict['Airmass']
+    mjd=header_dict['MJD']
+    resid_array = np.genfromtxt(resid_name)
+    max_index= np.argmax(sens_curve)
+    label= ','.join([resid_name, str(airmass), str(mjd)])
+    plt.plot(resid_array, label=label)
+    print("\n=============")
+    print(sens_name)
+    print('airmass:', airmass, 'mjd:', mjd)
+plt.xlabel(r'Pixel')
+spt.show_plot()
 
 tell1_array=  np.genfromtxt(tell_names[0], skip_header=1).T
 tell1_waves= tell1_array[0]

avg_spec.py
@@ -325,9 +325,10 @@ def make_rebin_avg_spec(target_list):
         rebin_fluxes.append(rebin_list[1])
         rebin_skies.append(rebin_list[2])
         rebin_noise2s.append(rebin_list[3])
+    
     avg_rebin_flux= np.nanmean(rebin_fluxes, axis=0)
     avg_rebin_sky= np.nanmean(rebin_skies, axis=0)
-    avg_rebin_noise2= np.nanmean(rebin_noise2s, axis=0)
+    avg_rebin_noise2= np.nanmean(rebin_noise2s, axis=0)**2/np.nansum(rebin_noise2s, axis=0)
     avg_rebin_noise= np.sqrt(avg_rebin_noise2)
     avg_rebin_noise=avg_rebin_noise/avg_rebin_flux
     hdu=fits.PrimaryHDU(ref_spec[0], header= ref_header)

plot_spec.py
@@ -55,7 +55,7 @@ sdss_path = '/Users/BenKaiser/Desktop/SDSS_speclib/'
 #sdss_path= sdss_path+'G0_K5/'
 #sdss_path = '/Users/BenKaiser/Desktop/SDSS_speclib/G0_K5/'
 #print(filenames)
-plot_wavelength=True
+plot_wavelength=False
 plot_400m2_tell= False
 #norm_range=[1240,1280]
 #norm_range=[1560,1590]
@@ -69,9 +69,10 @@ plot_400m2_tell= False
 #norm_range=[8058,8231]
 #norm_range=[5100,5400]
 #norm_range=[6090,6240]
-norm_range=[6640,6670]#20190530 400M1 norm range
+#norm_range=[6640,6670]#20190530 400M1 norm range
 #norm_range=[6630,6690]#wider double norm range
 #norm_range=[5740,5850]
+norm_range=[5270,5560]
 ####norm_range=np.array(norm_range)+wavelength_offset
 
 file_setting='all_avg'
@@ -108,7 +109,7 @@ if file_setting=='all_avg':
 
 elif file_setting=='all_wctb':
     print(file_setting)
-    filenames=glob('wctb*')
+    filenames=glob('wctb*aia*449_*')
     #filenames=glob('wctb*Feige110_*')
     #filenames=glob('wctb*aia*2320*')
     single_iterate=True
@@ -117,8 +118,8 @@ elif file_setting=='all_wctb':
 
 elif file_setting=='all_fwctb':
     print(file_setting)
-    filenames=glob('fwctb*LTT*')
-    #filenames=glob('fwctb*aia*1644*')
+    #filenames=glob('fwctb*LTT*')
+    filenames=glob('fwctb*aia*9_*')
     #filenames=glob('fwctb*2356*')
     single_iterate=True
     double_iterate=False
@@ -626,7 +627,7 @@ if single_iterate:
         #print(filename, 'mean: ', np.nanmean(target_spec[1]))
         #nu_spec= spt.flambda_to_fnu(target_spec, dlambda)
         #conv_spec= convolve_spectrum(target_spec, header)
-        plot_spectrum(target_spec, filename, header, smooth=True, norm=True, kernel_type='box')
+        plot_spectrum(target_spec, filename+str(header['airmass']), header, smooth=True, norm=True, kernel_type='box')
         #plot_spectrum(nu_spec, 'fnu', header, smooth=True, norm=False, kernel_type='box')
         #plot_spectrum(target_spec, filename, header, smooth=True, norm=True)
         #target_spec[1]=header['airmass']

spec_plot_tools.py
@@ -661,7 +661,7 @@ def rebin_spec(input_filename, desired_waves, desired_dlambda):
         
         rebin_flux_list.append(np.sum(target_flux*rebin_factors))
         rebin_sky_list.append(np.sum(sky* rebin_factors))
-        rebin_noise2_list.append(np.sum(noise2*rebin_factors))
+        rebin_noise2_list.append(np.sum(noise2*rebin_factors**2))
         
     #plt.plot(desired_waves, rebin_flux_list, label='rebinned spectrum')
     #plt.plot(target_spec[0], target_spec[1], label='original spectrum')

wave_cal.py
@@ -86,7 +86,7 @@ trace_offset =0#amount by which the calculated trace needs to be offset to end u
 #trace_band_mid= 95   #y-pixel that's about the center of the trace J1431
 #trace_band_mid=105 #y-pixel for Keaton's object 2019-03-07 2019-03-25 commented out
 #trace_band_mid=110
-trace_band_mid=95
+trace_band_mid=105
 #trace_band_mid= 112 #y-pixel for SDSSJ1159 400M1
 #trace_band_mid= 90 #y-pixel for SDSSJ1159 400M2
 #trace_band_mid=130 #
@@ -112,9 +112,10 @@ lamp_poly_degree=5
 flat_poly= 7
 #bkg_shift= 25 #2019-03-25 commented out
 #bkg_shift = 50 #20190412 previously in place
-bkg_shift= 30 #standard shift used
+#bkg_shift= 30 #standard shift used
 #bkg_shift=60
 #bkg_shift= 64
+bkg_shift=50
 #bkg_shift=55
 #bkg_core_sides= 2*core_sides #This should be changed most likely to make the value be higher to further reduce noise.
 bkg_side_multi= 1.5 #mutliple of core_sides that that  bkg_core_sides should be later
@@ -488,7 +489,7 @@ def get_trace_waves(target_med, lamp_im, do_wavelengths=True, poly_coeffs_lamp=[
         coll_seeing_sigmas= []
         rebin_counter= 0
         for pixel_column in rebinned_imT:
-            seeing_p0[1]=np.argmax(pixel_column)
+            #seeing_p0[1]=np.argmax(pixel_column)
             try:
                 if rebin_counter%25==26:
                     subset_popt, subset_pcov = fit_gaussian_curve(y_pos, pixel_column, seeing_p0, trace_band_width, plot_all=True, bounds = see_fit_bounds, fixed_width=False)
@@ -507,7 +508,7 @@ def get_trace_waves(target_med, lamp_im, do_wavelengths=True, poly_coeffs_lamp=[
                 max_fluxes.append(subset_popt[0])
                 coll_seeing_sigmas.append(subset_popt[2])
             else:
-                print("seeing sigma was on the low boundary so we're not including it")
+                print("seeing sigma was on the low boundary so we're not including it: x=", rebinned_x_positions[0][rebin_counter])
                 y_positions.append(np.nan)
                 max_fluxes.append(np.nan)
                 coll_seeing_sigmas.append(np.nan)
@@ -977,13 +978,13 @@ for counter, img in enumerate(speclist):
         target_light= target_light/header['EXPTIME'] #converting to counts/s
         bkg_light= bkg_light/header['EXPTIME'] #converting to counts/s
         
-        plt.plot(target_light, label='target avg bkg')
-        plt.plot(ctarget_light, label='target poly bkg')
-        plt.plot(bkg_light, label='avg bkg')
-        plt.plot(cbkg_light, label='poly bkg')
-        plt.legend()
-        plt.title('comparison of bkg methods')
-        plt.show()
+        #plt.plot(target_light, label='target avg bkg')
+        ##plt.plot(ctarget_light, label='target poly bkg')
+        #plt.plot(bkg_light, label='avg bkg')
+        ##plt.plot(cbkg_light, label='poly bkg')
+        #plt.legend()
+        #plt.title('comparison of bkg methods')
+        #plt.show()
         if (do_airglow_corr and setup_dict['air_corr']) :
             #airline_array= np.genfromtxt(cp.line_list_dir+ cp.airline_name, names = True, delimiter='\t')
             airline_array= Table.read(cp.line_list_dir+cp.airline_name, format='ascii.tab')