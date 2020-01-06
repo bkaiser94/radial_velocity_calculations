2a6a3b244303b7b25fd853b172c8dbfa1b88c8c2
Ben Kaiser <bkaiser94@gmail.com>
2020-01-06 14:23:39 -0500

    Attempt at implementation of the Hurley polynomials, but they're definitely wrong at the moment.

--- hurley_polynomials.py
+++ hurley_polynomials.py
@@ -21,3 +21,91 @@ import scipy.interpolate as scinterp
 
 import cal_params as cp
 from a_coeffs import a_coeffs
+
+
+default_z=0.02
+
+def make_match(value, array):
+    try:
+        output_val=np.ones(array.shape)*value
+    except AttributeError:
+        output_val=value
+    return output_val
+
+def zeta(z):
+    return np.log10(z/0.02)
+
+def a(z,index):
+    """
+    the a_n equation from the beginning of Appendix A of Hurley et al. 2000
+    
+    I just left out the mu term because none of these variables have a mu value, and I didn't bother putting it in the
+    a_coeffs list either as a result.
+    """
+    zeta_val=zeta(z)
+    a_index=a_coeffs[index]
+    a_out=a_index[0]+a_index[1]*zeta_val+a_index[2]*zeta_val**2+a_index[3]*zeta_val**3
+    #print('a_'+str(index),a_out)
+    return a_out
+
+def get_t_bgb(z,mass):
+    print('a_1',a(z,1))
+    print('a(z,2)*mass**4',a(z,2)*mass**4)
+    print('a(z,3)*mass**5.5 ',a(z,3)*mass**5.5 )
+    print('mass**7',mass**7)
+    print('(a(z,1)+a(z,2)*mass**4+a(z,3)*mass**5.5 + mass**7)',(a(z,1)+a(z,2)*mass**4+a(z,3)*mass**5.5 + mass**7))
+    print('a(z,4)*mass**2',a(z,4)*mass**2)
+    print('a(z,5)*mass**7',a(z,5)*mass**7)
+    print('(a(z,4)*mass**2 + a(z,5)*mass**7)',(a(z,4)*mass**2 + a(z,5)*mass**7))
+    return(a(z,1)+a(z,2)*mass**4+a(z,3)*mass**5.5 + mass**7)/(a(z,4)*mass**2 + a(z,5)*mass**7)
+
+def get_x(z):
+    first_val=make_match(0.95,z)
+    last_val=make_match(0.99, z)
+    return np.max([first_val,
+                   np.min([
+                       0.95-0.03*(zeta(z)+0.30103),
+                       last_val
+                       ],axis=0)],axis=0)
+
+def get_mu(z,mass):
+    print(np.max([a(z,6)/(mass**a(z,7)),a(z,8)+(a(z,9)/mass**(a(z,10)))],axis=0))
+    first_val=make_match(0.5, mass)
+    return np.max([
+        first_val,
+        1.0-0.01 * np.max([
+            a(z,6)/(mass**a(z,7)),
+            a(z,8)+(a(z,9)/(mass**a(z,10)))
+            ],axis=0)],axis=0)
+
+
+def get_t_ms(mass, z=default_z):
+    """
+    Default is what I'm pretty sure is solar metallicity
+    """
+    t_bgb=get_t_bgb(z,mass)
+    return np.max([
+        get_mu(z,mass)*t_bgb,
+        get_x(z)*t_bgb
+        ],axis=0)#*1e-3
+
+
+test_mass=1.
+print('zeta', zeta(default_z))
+print('t_bgb', get_t_bgb(default_z, test_mass))
+print('mu', get_mu(default_z, test_mass))
+print('x', get_x(default_z))
+print('t_ms',get_t_ms(test_mass))
+
+
+#mass_vals=np.linspace(0.1,13.,100)
+
+#t_ms_vals= get_t_ms(mass_vals)
+#t_bgb_vals=get_t_bgb(default_z, mass_vals)
+
+
+#plt.plot(np.log10(mass_vals), np.log10(t_ms_vals))
+#plt.plot(np.log10(mass_vals), np.log10(t_bgb_vals))
+#plt.show()
+
+
