d3d22f01f93a08761cf7c7e13da3f80805d7da59
Ben Kaiser <bkaiser94@gmail.com>
2020-08-10 14:23:19 -0400

    All file conversion changes besides calculating the delta lambda values in advance since later parts of my code expect that to already be there, so that still needs to be added.

--- gemini_to_goodman_fits.py
+++ gemini_to_goodman_fits.py
@@ -35,17 +35,63 @@ def convert_file(filename):
     hdu=fits.open(filename)
     data_all=hdu[0].data
     header=hdu[0].header
-    flux_opt=data_all[0,0,:]
-    wavelength=data_all[5,0,:]
-    new_filename=clean_filename(filename)
-    output_hdu=fits.PrimaryHDU(wavelength, header=header)
-    hdu1=fits.ImageHDU(flux_opt)
-    standin_ones=np.ones(flux_opt.shape)
-    hdu2=fits.ImageHDU(standin_ones)
-    hdu3=fits.ImageHDU(standin_ones)
-    hdu4=fits.ImageHDU(standin_ones)
-    hdulist=fits.HDUList([output_hdu,hdu1,hdu2,hdu3,hdu4])
-    hdulist.writeto(new_filename, overwrite=True)
+    print('opening ',filename)
+    try:
+        print('inside try statement')
+        if header['convertd']==True:
+            print('\n\n',filename, 'is already converted to Goodman format. No conversion needed. Moving on.\n\n')
+        else:
+            print(filename, "'convertd' header does exist, but somehow the conversion hasn't been done... This really should never print. \n\n\n****")
+    except KeyError:
+        print("Key error caught")
+        print('converting', filename)
+        header['comment']= 'The delta lambda values in extension 4  are not real at present'
+        header.append(card=('convertd', True, 'Gemini to Goodman conversion performed'))
+        try:
+            flux_opt=data_all[0,0,:]
+            wavelength=data_all[5,0,:]
+            background=data_all[2,0,:]
+            noise= data_all[3,0,:]
+            new_filename=clean_filename(filename)
+            #need to delete headers that are inaccurate in the new file, namely the things pointing to the labels of the spectrum
+            for index in range(1,7):
+                del header['BANDID'+str(index)]
+            
+            ### headers that need to be in the file for functions to work properly for flux calibration
+            #AIRMASS, which is already present in the way we would want it.
+            
+            #OPENTIME
+            header.append(card=('OPENTIME',header['UTSTART'],'time for shutter open in UTC'))
+            #OPENDATE
+            header.append(card=('OPENDATE',header['DATE-OBS'],'date for shutter open in UTC'))
+            #PIX_SCAL pixel scale of CCD with the binning applied, i.e. if default scale is 0.15"/pix but we binned 2x2, this value should be 0.3
+            header.append(card=('PIX_SCAL', header['PIXSCALE'],' "/pixel'))
+            #SEE_SIG sigma value of the gaussian in units of binned pixels
+            pixel_scale=float(header['PIXSCALE'])
+            see_FWHM=header['SPECFWHM'] #I realize that without the ADC and since we positioned at the parallactic, this value is most likely technically slightly too large for the actual seeing, but given that it's going to be used on a DC for the flux calibration anyway, it's not like it really matters that it's slightly too large.
+            seeing_sig=(see_FWHM*0.5)/np.sqrt(2*np.log(2.))
+            print("Seing FWHM:", see_FWHM)
+            print("Seeing Sigma:", seeing_sig)
+            print('Seeing in "', see_FWHM*pixel_scale)
+            header.append(card=("SEE_FWHM",see_FWHM,'FWHM of spectrum, approx. seeing in pixels'))
+            header.append(card=('SEE_SIG', seeing_sig, 'Sigma value of gaussian fit to spectrum trace in pixels'))
+            #SLIT the name of the slit that was used
+            header.append(card=('SLIT', header['MASKNAME'],'slit name'))
+            #EXPTIME this is already handled in the default headers of Gemini
+            
+            ####
+            
+            
+            output_hdu=fits.PrimaryHDU(wavelength, header=header)
+            hdu1=fits.ImageHDU(flux_opt)
+            standin_ones=np.ones(flux_opt.shape)
+            hdu2=fits.ImageHDU(background)
+            hdu3=fits.ImageHDU(noise)
+            hdu4=fits.ImageHDU(standin_ones)
+            hdulist=fits.HDUList([output_hdu,hdu1,hdu2,hdu3,hdu4])
+            hdulist.writeto(new_filename, overwrite=True)
+        except IndexError:
+            print("Presumably this is actually a file that was converted, but was done prior to implementing the header to track it",filename,'\n\n')
     return
 
 
@@ -56,6 +102,8 @@ def convert_file(filename):
 
 
 specfiles=glob.glob('*.ms.fits')
+#specfiles=glob.glob('*.fits')
+print('\n\n',specfiles,'\n\n')
 for fname in specfiles:
     print(fname)
     print(clean_filename(fname))
@@ -63,10 +111,16 @@ for fname in specfiles:
     hdu=fits.open(fname)
     data_all=hdu[0].data
     header=hdu[0].header
-    flux_opt=data_all[0,0,:]
-    wavelength=data_all[5,0,:]
-
     plt.figure()
-    plt.plot(wavelength,flux_opt)
+    try:
+        flux_opt=data_all[0,0,:]
+        wavelength=data_all[5,0,:]
+        plt.plot(wavelength,data_all[1,0,:],label='raw extracted spectrum')
+    except IndexError as error:
+        print("IndexError:", error)
+        print('Not compatible with plotting part of code at this point.')
+
+    plt.plot(wavelength,flux_opt, label='optimally extracted spectrum')
     plt.title(fname)
+    plt.legend()
     plt.show()
